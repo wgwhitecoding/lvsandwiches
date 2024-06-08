f3a1065b7cce1dd4d6e7d1d9106e33c010f56bb5
willcodinggod white <walidwhite@live.co.uk>
2024-06-08 13:18:29 +0000

    added
    added value diplay

--- run.py
+++ run.py
@@ -25,6 +25,7 @@ def get_sales_data():
     sales_data = data_str.split(",")
     validate_data(sales_data)
 
+
 def validate_data(values):
     """
     Inside the try, converts all string values into integers.
@@ -32,11 +33,13 @@ def validate_data(values):
     or if there aren't exactly 6 values.
     """
     try:
+        [int(value) for value in values]
         if len(values) != 6:
             raise ValueError(
                 f"Exactly 6 values required, you provided {len(values)}"
             )
     except ValueError as e:
         print(f"Invalid data: {e}, please try again.\n")
+
 
 get_sales_data()
